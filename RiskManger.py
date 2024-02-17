0b245f049b4dd17ae8a5a5c0a64c6fd7baced3ee
abdurislam <islamabdur15@gmail.com>
2024-02-17 13:50:02 -0800

    python

--- RiskManger.py
+++ RiskManger.py
@@ -1,6 +1,6 @@
 import math
 class person:
-    def __init__(self,name, gpa, social, greekBill, riskCount, lastRisk, isEiligible, attendence, moneyOwed, isPlege, isBrother, isExec, isSenior, isFormerExec):
+    def __init__(self,name, gpa, social, greekBill, riskCount, lastRisk, isEiligible, attendence, moneyOwed, isPlege, isBrother, isExec, isSenior, isFormerExec, programing, shitList):
         self.name = name
         self.gpa = gpa
         self.social = social
@@ -13,13 +13,18 @@ class person:
         self.isPlege = isPlege
         self.isBrother = isBrother
         self.isExec = isExec
+        self.programing = programing
+        self.shitList = shitList
 
     def riskEligibility(self):
         
          if self.isExec == False or self.isFormerExec == False:
              self.isEiligible = False
          else:
-             if self.social == False or self.greekBill == False:
+             if self.social == False or self.greekBill == False or self.gpa < 2.9:
+                 self.shitList = True
+                 self.isEiligible = True
+        
             
 
              
